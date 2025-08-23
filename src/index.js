87d22eda30b6725516b3b72a144cd4c54eed3a9a
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-23 19:28:52 +0500

    refactor: remove unused task creation and list manipulation code
    - Remove unnecessary task creation and list manipulation code from src/index.js
    - Delete task1 and task2 creation as they are no longer needed
    - Remove defaultList.addTask and defaultList.removeTask calls
    - Eliminate console.dir calls for defaultList.tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import { createTask } from "./task";
 import { defaultList, createList } from "./list";
-
-const task1 = createTask(`name`, `description`, `dueDate`, `priority`);
-const task2 = createTask(`name2`, `description2`, `dueDate2`, `priority2`);
-
-defaultList.addTask(task1);
-defaultList.addTask(task2);
-console.dir(defaultList.tasks);
-
-defaultList.removeTask(task1.id);
-console.dir(defaultList.tasks);
