48bd4fed13d06767824d8bdf38878217ba758c10
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-08-28 04:56:43 +0500

    feat(frontend): implement basic layout and styles for MyTasks app
    - Create new DOM manipulation functions for displaying lists
    - Set up initial state with default list and current list variables
    - Update CSS variables and class names for consistency
    - Modify HTML structure to reflect new layout and naming conventions
    - Add new header icon and toggle off icon
    - Update sidebar and main content styling and structure

---
+++ src/dom.js
@@ -0,0 +1,5 @@
+function displayLists(listsArray) {
+  const listsContainer = document.querySelector(`#listsContainer`);
+  const listItem = document.createElement(`li`);
+  listItem.classList.add(`sidebar__item`);
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import "./styles.css";
 import { createTask } from "./task";
 import { defaultList, createList } from "./list";
 
+export const lists = [defaultList];
+export const currentList = defaultList;
+
 const dialog = document.querySelector(`dialog`);
 // dialog.showModal();
