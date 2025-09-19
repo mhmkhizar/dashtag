e07e63f815568b71cbf5c44fc3ee7000f2ff33f0
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-09-19 17:42:46 +0500

    refactor(ui): improve filter list item icon creation
    - Replace conditional icon creation with a more concise approach
    - Remove unnecessary isStarredTasksProject function
    - Use ternary operator to set icon classes and text content

--- src/ui/sidebar/filter-list-item.js
+++ src/ui/sidebar/filter-list-item.js
@@ -6,31 +6,20 @@ export function generate(item) {
     classes: `filter-list-item flex h-8 cursor-pointer items-center gap-2 rounded-[var(--radius)] px-4 hover:bg-current/10`,
     attributes: { "data-projectid": `${item.id}` },
   });
+  const iconSpan = Helper.createElement({
+    element: `span`,
+    classes:
+      item.id === `starred-tasks-project`
+        ? `filter-item-icon icon material-symbols-rounded icon-wght-300`
+        : `filter-item-icon icon material-symbols-rounded icon-wght-300 !text-2xl`,
+    textContent: item.id === `starred-tasks-project` ? `star` : `check`,
+  });
   const textSpan = Helper.createElement({
     element: `span`,
     classes: `filter-item-text`,
     textContent: `${item.title}`,
   });
 
-  let iconSpan;
-  if (isStarredTasksProject(item)) {
-    iconSpan = Helper.createElement({
-      element: `span`,
-      classes: `filter-item-icon icon material-symbols-rounded icon-wght-300`,
-      textContent: `star`,
-    });
-  } else {
-    iconSpan = Helper.createElement({
-      element: `span`,
-      classes: `filter-item-icon icon material-symbols-rounded icon-wght-300 !text-2xl`,
-      textContent: `check`,
-    });
-  }
-
   itemLi.append(iconSpan, textSpan);
   return itemLi;
-}
-
-function isStarredTasksProject(item) {
-  return item.id === `starred-tasks-project`;
 }
